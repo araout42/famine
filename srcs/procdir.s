918e867ba8ad6e0d6b25c843fb81963a5e3b002d
May <>
2021-06-05 13:44:41 -0400

    procdir almost worksszzzssz

--- srcs/procdir.s
+++ srcs/procdir.s
@@ -7,7 +7,7 @@ global _start
 _start:
 PUSH
 mov rbp, rsp
-sub rbp, 5000
+sub rbp, 1200
 
 .opendir:
 	lea rdi, [rel pdir]
@@ -48,10 +48,10 @@ read_file:
 	movzx edx, word[rdi+dirent.d_reclen]
 	mov al, byte[rdi + rdx - 1]
 	add rdi, dirent.d_name
-	call .open_comm
 	add r13, rdx
 	cmp al, DT_DIR
 	jne .nextfile
+
 
 
 .open_comm:
@@ -62,23 +62,72 @@ xor rsi, rsi
 	mov al, byte[r11]
 	mov byte[rel commpath + rsi], al
 	inc rsi
-	cmp byte[r11], 0
+	cmp rsi, 6
 	inc r11
-	jnz .dirname
+	jb .dirname
 
 .filename:
-	mov al, byte [rdi]
-	mov byte[rsi], al
+	mov al, byte[rdi]
+	mov byte[rel commpath + rsi], al
 	inc rsi
 	inc rdi
 	cmp byte[rdi], 0
-	jnz .filename
+	jne .filename
+
+lea r11, [rel pname]
+mov rdx, 0
+.commfile:
+	mov al, byte[r11]
+	mov byte[rel commpath + rsi], al
+	inc r11
+	inc rsi
+	inc rdx
+	cmp rdx, 5
+	jne .commfile
+	mov byte[rel commpath + rsi], 0
+
+	mov rax, _open
+	lea rdi, [rel commpath]
+	mov rdx, O_RDWR
+	syscall
+	test eax, eax
+	jl .nextfile
+
+	mov rdi, rax
+	mov rax, _read
+	lea rsi, [rel thename]
+	mov rdx, 90
+	syscall
+	test eax, eax
+	jl .close
+	push rdi
+	mov byte[rsi + rax - 1], 0x0a
+	mov rdx, rax
+	mov rax, 1
+	mov rdi, 1
+	syscall
+	test eax, eax
+	pop rdi
+	jl .close
+	xor rsi, rsi
+	.loop:
+	mov dl, byte[forbidden + rsi]
+	cmp byte[rel thename + rsi], dl
+	inc rsi
+	je .loop
+
+	.close:
+	mov rax, _close
+	syscall
+	jmp .nextfile
 
 .nextfile:
 cmp r13, r12
 jl read_file
 ret
 
-pdir db "/proc/", 0, 0
+forbidden db "test.out", 0
+pdir db "/proc/", 0, 0, 0, 0
 pname db "/comm"
-commpath	db 0 dup(100)
+commpath times 100 db  0
+thename times 100 db 0
---
+++ test.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <unistd.h>
+
+
+int main(void)
+{
+	while (1)
+	{
+		sleep(100);
+	}
+	exit(1);
+}
